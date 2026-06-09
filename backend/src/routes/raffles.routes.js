49939d038241024591f5d23a661fb3e643e90162
VictorAlejandroJanco <victorjanco1992@gmail.com>
2026-06-09 10:13:58 -0300

    Update raffle routes to include startDraw and confirmWinner

--- backend/src/routes/raffles.routes.js
+++ backend/src/routes/raffles.routes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import {
   getRaffles, getRaffle, getLastRaffle, createRaffle,
-  activateRaffle, performDraw, resetRaffle, getUserRaffleNumbers,
-  updateRaffle, deleteRaffle, getMyWin,
+  activateRaffle, startDraw, confirmWinner, resetRaffle,
+  getUserRaffleNumbers, updateRaffle, deleteRaffle, getMyWin,
   notifyRaffleStarting, notifyRaffleSpinning,
   getLiveStatus, getBroadcast,
 } from '../controllers/raffles.controller.js';
@@ -22,7 +22,8 @@ rafflesRouter.post('/', authenticate, requireAdmin, createRaffle);
 rafflesRouter.put('/:id', authenticate, requireAdmin, updateRaffle);
 rafflesRouter.delete('/:id', authenticate, requireAdmin, deleteRaffle);
 rafflesRouter.patch('/:id/activate', authenticate, requireAdmin, activateRaffle);
-rafflesRouter.post('/:id/draw', authenticate, requireAdmin, performDraw);
+rafflesRouter.post('/:id/draw', authenticate, requireAdmin, startDraw);
+rafflesRouter.post('/:id/confirm-winner', authenticate, requireAdmin, confirmWinner);
 rafflesRouter.post('/:id/reset', authenticate, requireAdmin, resetRaffle);
 rafflesRouter.post('/:id/notify-starting', authenticate, requireAdmin, notifyRaffleStarting);
 rafflesRouter.post('/:id/notify-spinning', authenticate, requireAdmin, notifyRaffleSpinning);
